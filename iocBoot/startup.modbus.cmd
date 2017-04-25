87cea2e8cef3b68fc34f368c576455a8cbcb45cb
gwbrown <gwbrown@slac.stanford.edu>
2017-04-25 15:32:54 -0700

    Moving to base 3-15, updating modules.

--- iocBoot/startup.modbus.cmd
+++ iocBoot/startup.modbus.cmd
@@ -9,6 +9,7 @@
 #			 int noProcessEos);
 
 drvAsynIPPortConfigure("$(P)", "$(IP):502", 0,0,1)
+dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN,PORT=$(P),ADDR=0,IMAX=0,OMAX=0")
 
 # modbusInterposeConfig(const char *portName, modbusLinkType linkType,
 #			int timeoutMsec, int writeDelayMsec)
@@ -22,11 +23,13 @@ modbusInterposeConfig("$(P)", 0, 2000, 0)
 #			modbusFunctions, modbusStartAddress, modbusLength,
 #			dataType, pollMesec, "plcType")db
 drvModbusAsynConfigure("RF1_In_Word", "$(P)", 0, 3, 1, 125, 0, 100, "RK")
+dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_IN,PORT=RF1_In_Word,ADDR=0,IMAX=0,OMAX=0")
 
 # Access 5 words as outputs 
 # Function code = 6 (single register)
 # Default data type unsigned integer
 drvModbusAsynConfigure("RF1_Out_Word", "$(P)", 0, 6, 1, 10, 1, 100, "RK")
+dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_OUT,PORT=RF1_Out_Word,ADDR=0,IMAX=0,OMAX=0")
 
 # Enable ASYN_TRACEIO_HEX on sever
 asynSetTraceIOMask("$(P)", 0,4)
