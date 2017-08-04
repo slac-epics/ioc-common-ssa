37f4d4667ac329083eb4864115c2f5d561ba54c8
Garth Brown <gwbrown@slac.stanford.edu>
2017-08-04 15:53:11 -0700

    Moving to the new LCLS-II standard EPICS base, 3-15

--- iocBoot/startup.modbus.cmd
+++ iocBoot/startup.modbus.cmd
@@ -13,7 +13,7 @@ dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN,PORT=$(P),ADDR=0,IMAX=0,OMAX=0")
 
 # modbusInterposeConfig(const char *portName, modbusLinkType linkType,
 #			int timeoutMsec, int writeDelayMsec)
-modbusInterposeConfig("$(P)", 0, 2000, 0)
+modbusInterposeConfig("$(P)", 0, 300, 0)
 
 
 # Word Access at Modbus address 1
@@ -22,20 +22,18 @@ modbusInterposeConfig("$(P)", 0, 2000, 0)
 # drvModbusAsynConfigure("portName", "TcpPortName", slaveaddress,
 #			modbusFunctions, modbusStartAddress, modbusLength,
 #			dataType, pollMesec, "plcType")db
-drvModbusAsynConfigure("RF1_In_Word", "$(P)", 0, 3, 1, 125, 0, 100, "RK")
+drvModbusAsynConfigure("RF1_In_Word", "$(P)", 0, 3, 1, 117, 0, 350, "RK")
 dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_IN,PORT=RF1_In_Word,ADDR=0,IMAX=0,OMAX=0")
 
 # Access 5 words as outputs 
 # Function code = 6 (single register)
 # Default data type unsigned integer
-drvModbusAsynConfigure("RF1_Out_Word", "$(P)", 0, 6, 1, 10, 1, 100, "RK")
+drvModbusAsynConfigure("RF1_Out_Word", "$(P)", 0, 6, 1, 6, 1, 100, "RK")
 dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_OUT,PORT=RF1_Out_Word,ADDR=0,IMAX=0,OMAX=0")
 
 # Enable ASYN_TRACEIO_HEX on sever
-asynSetTraceIOMask("$(P)", 0,4)
+asynSetTraceIOMask("$(P)ASYN", 0,4)
+asynSetTraceIOMask("$(P)ASYN_IN", 0,4)
+asynSetTraceIOMask("$(P)ASYN_OUT", 0,4)
 
  dbLoadRecords("db/ssa.db", "P=$(P)")
-
-
-
-
