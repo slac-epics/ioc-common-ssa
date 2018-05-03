8e0d57d621f775cf3b48aeab1ae074159c9fed23
Michael Dunning <mdunning@slac.stanford.edu>
2018-05-02 17:21:43 -0700

    Support multiple CA1300 SSAs per IOC

--- iocBoot/sioc-b15-ssa1/st.cmd
+++ iocBoot/sioc-b15-ssa1/st.cmd
@@ -1,10 +1,6 @@
 #!../../bin/rhel6-x86_64/ssa
 
-## You may have to change ssa to something else
-## everywhere it appears in this file
-
 < envPaths
-
 
 cd ${TOP}
 
@@ -13,61 +9,47 @@ dbLoadDatabase("dbd/ssa.dbd")
 ssa_registerRecordDeviceDriver(pdbbase)
 
 
-# ====================================================================
-# Setup some additional environment variables
-# ====================================================================
-# Setup environment variables
+## Set up environment variables
 epicsEnvSet("ENGINEER", "Garth Brown")
 epicsEnvSet("LOCATION", "B15 SSA test stand 1")
-epicsEnvSet("P",        "ACCL:L1B:0210:SSA:")
-epicsEnvSet("IP",       "ssa-b15-rf0110")
 epicsEnvSet("IOC_P",    "SIOC:B15:SSA1")
 
-# tag log messages with IOC name
+## Tag log messages with IOC name
 # How to escape the "iocb15-rf01" as the PERL program
 # will try to repplace it.
 # So, uncomment the following and remove the backslash
 epicsEnvSet("EPICS_IOC_LOG_CLIENT_INET","${IOC}")
 
-# ========================================================
-# Support Large Arrays/Waveforms; Number in Bytes
-# Please calculate the size of the largest waveform
-# that you support in your IOC.  Do not just copy numbers
-# from other apps.  This will only lead to an exhaustion
-# of resources and problems with your IOC.
-# The default maximum size for a channel access array is
-# 16K bytes.
-# ========================================================
-#epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "65536")
 
-# END: Additional environment variables
-# ====================================================================
+## The following two environment variables should be set for each SSA
+# epicsEnvSet("P",        "<pv_prefix>")
+# epicsEnvSet("IP",       "<ssa_ip_address>")
+#
+## Source the following file for each SSA after setting the two environment variables P and IP
+# < iocBoot/startup.modbus_RK_CA1300.cmd
+#
+## Load record instances for each SSA, after setting the environment variable P
+# dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
 
-
-########################################################################
-# BEGIN: Hardware specific configuration
-#######################################################################
-
-# Uses environment variables $P and $IP, set above, to connect to the ssa
+# SSA1: L1B:0210
+epicsEnvSet("P",        "ACCL:L1B:0210:SSA:")
+epicsEnvSet("IP",       "ssa-b15-rf0110")
 < iocBoot/startup.modbus_RK_CA1300.cmd
+dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
+epicsThreadSleep(0.5)
 
-# END: Hardware specific configuration
-# =====================================================================
+# SSA2: L1B:0220
+epicsEnvSet("P",        "ACCL:L1B:0220:SSA:")
+epicsEnvSet("IP",       "ssa-b15-rf0120")
+< iocBoot/startup.modbus_RK_CA1300.cmd
+dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
+epicsThreadSleep(0.5)
 
-
-########################################################################
-# BEGIN: Load the record databases
-#######################################################################
 # =====================================================================
 # Load iocAdmin databases to support IOC Health and monitoring
 # =====================================================================
 dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC_P}")
 dbLoadRecords("db/iocAdminScanMon.db","IOC=${IOC_P}")
-
-# The following database is a result of a python parser
-# which looks at RELEASE_SITE and RELEASE to discover
-# versions of software your IOC is referencing
-# The python parser is part of iocAdmin
 dbLoadRecords("db/iocRelease.db","IOC=${IOC_P}")
 
 # =====================================================================
@@ -75,15 +57,6 @@ dbLoadRecords("db/iocRelease.db","IOC=${IOC_P}")
 # =====================================================================
 dbLoadRecords("db/save_restoreStatus.db", "P=${IOC_P}:")
 
-
-# =====================================================================
-#Load Additional databases:
-# =====================================================================
-## Load record instances
-dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
-
-# END: Loading the record databases
-########################################################################
 
 # =====================================================================
 ## Begin: Set up autosave/restore
@@ -140,12 +113,20 @@ caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
 caPutLogShow(2)
 # =====================================================
 
-## Start any sequence programs
-seq(seq_ssa_CA1300, "P=$(P)")
+## Start sequence programs, one for each SSA
+# This must be done after iocInit
+#
+# SSA1: L1B:0210
+seq(seq_ssa_CA1300, "P=ACCL:L1B:0210:SSA:")
+epicsThreadSleep(0.5)
+
+# SSA2: L1B:0220
+seq(seq_ssa_CA1300, "P=ACCL:L1B:0220:SSA:")
+epicsThreadSleep(0.5)
 
 ## Start autosave process:
 cd("${IOC_DATA}/${IOC}/autosave-req")
 makeAutosaveFiles()
-create_monitor_set("info_settings.req",20,"")
+create_monitor_set("info_settings.req", 20, "")
 
 
--- iocBoot/startup.modbus_RK_CA1300.cmd
+++ iocBoot/startup.modbus_RK_CA1300.cmd
@@ -9,7 +9,7 @@
 #			 int noProcessEos);
 
 drvAsynIPPortConfigure("$(P)", "$(IP):502", 0,0,1)
-dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN,PORT=$(P),ADDR=0,IMAX=0,OMAX=0")
+dbLoadRecords("db/asynRecord.db","P=$(P), R=ASYN, PORT=$(P), ADDR=0,IMAX=0,OMAX=0")
 
 # modbusInterposeConfig(const char *portName, modbusLinkType linkType,
 #			int timeoutMsec, int writeDelayMsec)
@@ -24,30 +24,32 @@ modbusInterposeConfig("$(P)", 0, 1500, 0)
 # drvModbusAsynConfigure("portName", "TcpPortName", slaveaddress,
 #			modbusFunctions, modbusStartAddress, modbusLength,
 #			dataType, pollMsec, "plcType")
-drvModbusAsynConfigure("RF1_In_Word", "$(P)", 0, 3, 1, 125, 0, 500, "RK")
-dbLoadRecords("db/asynRecord.db", "P=$(P),R=ASYN_IN,PORT=RF1_In_Word,ADDR=0,IMAX=0,OMAX=0")
-dbLoadRecords("db/statistics.template", "P=$(P),R=ASYN_IN,PORT=RF1_In_Word,SCAN=10 second")
+#
+# For multiple SSAs per IOC, need to use a portName that depends on $(P)
+drvModbusAsynConfigure("$(P)_inw1", "$(P)", 0, 3, 1, 125, 0, 500, "RK")
+dbLoadRecords("db/asynRecord.db", "P=$(P), R=ASYN_IN, PORT=$(P)_inw1, ADDR=0,IMAX=0,OMAX=0")
+dbLoadRecords("db/statistics.template", "P=$(P), R=ASYN_IN, PORT=$(P)_inw1, SCAN=10 second")
 
 # Modbus function code: 3 (Read Holding Registers, 16-bit word access)
 # Start address: 120
 # Access 8 words as inputs
 # Default data type: unsigned integer
-drvModbusAsynConfigure("RF2_In_Word", "$(P)", 0, 3, 120, 8, 0, 5000, "RK")
-dbLoadRecords("db/asynRecord.db", "P=$(P),R=ASYN_IN,PORT=RF2_In_Word,ADDR=0,IMAX=0,OMAX=0")
+drvModbusAsynConfigure("$(P)_inw2", "$(P)", 0, 3, 120, 8, 0, 5000, "RK")
+dbLoadRecords("db/asynRecord.db", "P=$(P), R=ASYN_IN, PORT=$(P)_inw2, ADDR=0,IMAX=0,OMAX=0")
 
 # Modbus function code: 6 (Write Single Register, 16-bit word access)
 # Start address: 1
 # Access 6 words as outputs 
 # Default data type: unsigned integer
-drvModbusAsynConfigure("RF1_Out_Word", "$(P)", 0, 6, 1, 6, 0, 100, "RK")
-dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_OUT,PORT=RF1_Out_Word,ADDR=0,IMAX=0,OMAX=0")
+drvModbusAsynConfigure("$(P)_outw1", "$(P)", 0, 6, 1, 6, 0, 100, "RK")
+dbLoadRecords("db/asynRecord.db","P=$(P), R=ASYN_OUT, PORT=$(P)_outw1, ADDR=0,IMAX=0,OMAX=0")
 
 # Modbus function code: 6 (Write Single Register, 16-bit word access)
 # Start address: 112
 # Access 5 words as outputs 
 # Default data type: unsigned integer
-drvModbusAsynConfigure("RF2_Out_Word", "$(P)", 0, 6, 112, 5, 0, 100, "RK")
-dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_OUT,PORT=RF2_Out_Word,ADDR=0,IMAX=0,OMAX=0")
+drvModbusAsynConfigure("$(P)_outw2", "$(P)", 0, 6, 112, 5, 0, 100, "RK")
+dbLoadRecords("db/asynRecord.db","P=$(P), R=ASYN_OUT, PORT=$(P)_outw2, ADDR=0,IMAX=0,OMAX=0")
 
 # Enable ASYN_TRACEIO_HEX on octet server
 #asynSetTraceIOMask("$(P)", 0, 4)
@@ -55,10 +57,10 @@ dbLoadRecords("db/asynRecord.db","P=$(P),R=ASYN_OUT,PORT=RF2_Out_Word,ADDR=0,IMA
 #asynSetTraceMask("$(P)", 0, 9)
 
 # Enable ASYN_TRACEIO_HEX on modbus server
-#asynSetTraceIOMask("RF1_In_Word", 0, 4)
+#asynSetTraceIOMask("$(P)_inw1", 0, 4)
 # Enable all debugging on modbus server
-#asynSetTraceMask("RF1_In_Word", 0, 255)
+#asynSetTraceMask("$(P)_inw1", 0, 255)
 # Dump up to 512 bytes in asynTrace
-asynSetTraceIOTruncateSize("RF1_In_Word", 0, 512)
+asynSetTraceIOTruncateSize("$(P)_inw1", 0, 512)
 
 
