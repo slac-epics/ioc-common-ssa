18e09a8a7af9020875159f5fb40110b749fee53c
Michael Dunning <mdunning@slac.stanford.edu>
2019-01-14 15:35:21 -0800

    Add support for 3.9 GHz model CA3900 SSA

---
+++ iocBoot/common/startup.modbus_RK_CA3900.cmd
@@ -0,0 +1,66 @@
+# Modbus config for R&K CA3900 SSA
+# Macros:
+# P - the PV name prefix
+# IP - The IP address or host name 
+
+# Use the following commands for TCP/IP
+# drvAsynIPPortConfigure(const char *portName, const char *hostInfo,
+# 			 unsigned int priority, int noAutoConnect,
+#			 int noProcessEos);
+
+drvAsynIPPortConfigure("$(P)", "$(IP):502", 0,0,1)
+dbLoadRecords("db/asynRecord.db","P=$(P), R=ASYN, PORT=$(P), ADDR=0,IMAX=0,OMAX=0")
+
+# modbusInterposeConfig(const char *portName, modbusLinkType linkType,
+#			int timeoutMsec, int writeDelayMsec)
+# linkType 0 = TCP/IP
+modbusInterposeConfig("$(P)", 0, 1500, 0)
+
+
+# Modbus function code: 3 (Read Holding Registers, 16-bit word access)
+# Start address: 1
+# Access 125 words as inputs
+# Default data type: unsigned integer
+# drvModbusAsynConfigure("portName", "TcpPortName", slaveaddress,
+#			modbusFunctions, modbusStartAddress, modbusLength,
+#			dataType, pollMsec, "plcType")
+#
+# For multiple SSAs per IOC, need to use a portName that depends on $(P)
+drvModbusAsynConfigure("$(P)_inw1", "$(P)", 0, 3, 1, 125, 0, 500, "RK")
+dbLoadRecords("db/asynRecord.db", "P=$(P), R=ASYN_IN, PORT=$(P)_inw1, ADDR=0,IMAX=0,OMAX=0")
+dbLoadRecords("db/statistics.template", "P=$(P), R=ASYN_IN, PORT=$(P)_inw1, SCAN=10 second")
+
+# Modbus function code: 3 (Read Holding Registers, 16-bit word access)
+# Start address: 125
+# Access 36 words as inputs
+# Default data type: unsigned integer
+drvModbusAsynConfigure("$(P)_inw2", "$(P)", 0, 3, 125, 36, 0, 5000, "RK")
+dbLoadRecords("db/asynRecord.db", "P=$(P), R=ASYN_IN, PORT=$(P)_inw2, ADDR=0,IMAX=0,OMAX=0")
+
+# Modbus function code: 6 (Write Single Register, 16-bit word access)
+# Start address: 1
+# Access 7 words as outputs 
+# Default data type: unsigned integer
+drvModbusAsynConfigure("$(P)_outw1", "$(P)", 0, 6, 1, 7, 0, 100, "RK")
+dbLoadRecords("db/asynRecord.db","P=$(P), R=ASYN_OUT, PORT=$(P)_outw1, ADDR=0,IMAX=0,OMAX=0")
+
+# Modbus function code: 6 (Write Single Register, 16-bit word access)
+# Start address: 128
+# Access 5 words as outputs 
+# Default data type: unsigned integer
+drvModbusAsynConfigure("$(P)_outw2", "$(P)", 0, 6, 128, 5, 0, 100, "RK")
+dbLoadRecords("db/asynRecord.db","P=$(P), R=ASYN_OUT, PORT=$(P)_outw2, ADDR=0,IMAX=0,OMAX=0")
+
+# Enable ASYN_TRACEIO_HEX on octet server
+#asynSetTraceIOMask("$(P)", 0, 4)
+# Enable ASYN_TRACE_ERROR and ASYN_TRACEIO_DRIVER on octet server
+#asynSetTraceMask("$(P)", 0, 9)
+
+# Enable ASYN_TRACEIO_HEX on modbus server
+#asynSetTraceIOMask("$(P)_inw1", 0, 4)
+# Enable all debugging on modbus server
+#asynSetTraceMask("$(P)_inw1", 0, 255)
+# Dump up to 512 bytes in asynTrace
+asynSetTraceIOTruncateSize("$(P)_inw1", 0, 512)
+
+
--- iocBoot/sioc-b15-ssa3/st.cmd
+++ iocBoot/sioc-b15-ssa3/st.cmd
@@ -1,10 +1,6 @@
 #!../../bin/rhel6-x86_64/ssa
 
-## You may have to change ssa to something else
-## everywhere it appears in this file
-
 < envPaths
-
 
 cd ${TOP}
 
@@ -13,61 +9,43 @@ dbLoadDatabase("dbd/ssa.dbd")
 ssa_registerRecordDeviceDriver(pdbbase)
 
 
-# ====================================================================
-# Setup some additional environment variables
-# ====================================================================
-# Setup environment variables
+## Set up environment variables
 epicsEnvSet("ENGINEER", "M. Dunning")
-epicsEnvSet("LOCATION", "B15 SSA test stand")
-epicsEnvSet("P",        "SSA3:B15:")
-epicsEnvSet("IP",       "ssa-b15-rf0130")
+epicsEnvSet("LOCATION", "B15 SSA test stand 3")
 epicsEnvSet("IOC_P",    "SIOC:B15:SSA3")
 
-# tag log messages with IOC name
-# How to escape the "iocb15-rf01" as the PERL program
-# will try to repplace it.
-# So, uncomment the following and remove the backslash
+## Tag log messages with IOC name
 epicsEnvSet("EPICS_IOC_LOG_CLIENT_INET","${IOC}")
 
-# ========================================================
-# Support Large Arrays/Waveforms; Number in Bytes
-# Please calculate the size of the largest waveform
-# that you support in your IOC.  Do not just copy numbers
-# from other apps.  This will only lead to an exhaustion
-# of resources and problems with your IOC.
-# The default maximum size for a channel access array is
-# 16K bytes.
-# ========================================================
-#epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "65536")
+## The following two environment variables should be set for each SSA
+# epicsEnvSet("P",        "<pv_prefix>")
+# epicsEnvSet("IP",       "<ssa_ip_address>")
+#
+## Source the following file for each SSA after setting the two environment variables P and IP
+# < iocBoot/startup.modbus_RK_CA1300.cmd
+#
+## Load record instances for each SSA, after setting the environment variable P
+# dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
 
-# END: Additional environment variables
-# ====================================================================
+# SSA1: SSA:B15:003 
+epicsEnvSet("P",        "SSA:B15:003:")
+epicsEnvSet("IP",       "ssa-b15-rf0130")
+< iocBoot/common/startup.modbus_RK_CA1300.cmd
+dbLoadRecords("db/ssa_RK_CA3900.db", "P=$(P)")
+epicsThreadSleep(0.5)
 
+## SSA2: L1B:0220
+#epicsEnvSet("P",        "ACCL:L1B:0220:SSA:")
+#epicsEnvSet("IP",       "ssa-b15-rf0120")
+#< iocBoot/common/startup.modbus_RK_CA1300.cmd
+#dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
+#epicsThreadSleep(0.5)
 
-########################################################################
-# BEGIN: Hardware specific configuration
-#######################################################################
-
-# Uses environment variables $P and $IP, set above, to connect to the ssa
-< iocBoot/startup.modbus_RK_CA186.cmd
-
-# END: Hardware specific configuration
-# =====================================================================
-
-
-########################################################################
-# BEGIN: Load the record databases
-#######################################################################
 # =====================================================================
 # Load iocAdmin databases to support IOC Health and monitoring
 # =====================================================================
 dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC_P}")
 dbLoadRecords("db/iocAdminScanMon.db","IOC=${IOC_P}")
-
-# The following database is a result of a python parser
-# which looks at RELEASE_SITE and RELEASE to discover
-# versions of software your IOC is referencing
-# The python parser is part of iocAdmin
 dbLoadRecords("db/iocRelease.db","IOC=${IOC_P}")
 
 # =====================================================================
@@ -77,49 +55,39 @@ dbLoadRecords("db/save_restoreStatus.db", "P=${IOC_P}:")
 
 
 # =====================================================================
-#Load Additional databases:
-# =====================================================================
-## Load record instances
-dbLoadRecords("db/ssa_RK_CA186.db", "P=$(P)")
-dbLoadRecords("db/ssa_RK_CA186_modules.db", "P=$(P)")
-
-# END: Loading the record databases
-########################################################################
-
-# =====================================================================
-## Begin: Setup autosave/restore
+## Begin: Set up autosave/restore
 # =====================================================================
 
 # ============================================================
 # If all PVs don't connect continue anyway
 # ============================================================
-#save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_IncompleteSetsOk(1)
 
 # ============================================================
 # created save/restore backup files with date string
 # useful for recovery.
 # ============================================================
-#save_restoreSet_DatedBackupFiles(1)
+save_restoreSet_DatedBackupFiles(1)
 
 # ============================================================
 # Where to find the list of PVs to save
 # ============================================================
-#set_requestfile_path("${IOC_DATA}/${IOC}/autosave-req")
+set_requestfile_path("${IOC_DATA}/${IOC}/autosave-req")
 
 # ============================================================
 # Where to write the save files that will be used to restore
 # ============================================================
-#set_savefile_path("${IOC_DATA}/${IOC}/autosave")
+set_savefile_path("${IOC_DATA}/${IOC}/autosave")
 
 # ============================================================
 # Prefix that is use to update save/restore status database
 # records
 # ============================================================
-#save_restoreSet_status_prefix("${IOC_P}:")
+save_restoreSet_status_prefix("${IOC_P}:")
 
 ## Restore datasets
-#set_pass0_restoreFile("info_settings.sav")
-#set_pass1_restoreFile("info_settings.sav")
+set_pass0_restoreFile("info_settings.sav")
+set_pass1_restoreFile("info_settings.sav")
 
 # =====================================================================
 # End: Setup autosave/restore
@@ -130,21 +98,31 @@ dbLoadRecords("db/ssa_RK_CA186_modules.db", "P=$(P)")
 # This is required if you use caPutLog.
 # Set access security file
 # Load common LCLS Access Configuration File
-#< ${ACF_INIT}
+< ${ACF_INIT}
 
 iocInit()
 
 # =====================================================
 # Turn on caPutLogging:
 # Log values only on change to the iocLogServer:
-#caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
-#caPutLogShow(2)
+caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+caPutLogShow(2)
 # =====================================================
 
+## Start sequence programs, one for each SSA
+# This must be done after iocInit
+#
+# SSA1: SSA:B15:003
+seq(seq_ssa_CA1300, "P=SSA:B15:003:")
+epicsThreadSleep(0.5)
+
+# SSA2: L1B:0220
+#seq(seq_ssa_CA1300, "P=ACCL:L1B:0220:SSA:")
+#epicsThreadSleep(0.5)
 
 ## Start autosave process:
-#cd("${IOC_DATA}/${IOC}/autosave-req")
-#makeAutosaveFiles()
-#create_monitor_set("info_settings.req",20,"")
+cd("${IOC_DATA}/${IOC}/autosave-req")
+makeAutosaveFiles()
+create_monitor_set("info_settings.req", 20, "")
 
 
