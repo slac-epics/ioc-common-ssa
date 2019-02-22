5376553c4800be04ec07c6cbbd9a1a176bbce751
Michael Dunning <mdunning@slac.stanford.edu>
2019-02-22 09:47:19 -0800

    Update ssa2 to 4.6kW

--- iocBoot/sioc-b15-ssa2/st.cmd
+++ iocBoot/sioc-b15-ssa2/st.cmd
@@ -32,7 +32,7 @@ epicsEnvSet("EPICS_IOC_LOG_CLIENT_INET","${IOC}")
 epicsEnvSet("P",        "ACCL:L1B:0220:SSA:")
 epicsEnvSet("IP",       "ssa-b15-rf0120")
 < iocBoot/common/startup.modbus_RK_CA1300.cmd
-dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
+dbLoadRecords("db/ssa_RK_CA1300_4.6kW.db", "P=$(P)")
 epicsThreadSleep(0.5)
 
 # =====================================================================
