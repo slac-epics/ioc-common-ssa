515c45b7edc0daeb8bfc68bf51f02db5b8e3cf3d
Michael Dunning <mdunning@slac.stanford.edu>
2020-07-23 10:54:16 -0700

    Switch CA1300_4.6kW to new iocsh format

--- iocBoot/sioc-b15-ssa1/st.cmd
+++ iocBoot/sioc-b15-ssa1/st.cmd
@@ -2,97 +2,17 @@
 
 < envPaths
 
-cd ${TOP}
-
-## Register all support components
-dbLoadDatabase("dbd/ssa.dbd")
-ssa_registerRecordDeviceDriver(pdbbase)
-
-
-## Set up environment variables
-epicsEnvSet("ENGINEER", "Mike Dunning")
+## Environment variables
 epicsEnvSet("LOCATION", "B15 SSA test stand 1")
-epicsEnvSet("IOC_P",    "SIOC:B15:SSA1")
+epicsEnvSet("IOC_NAME", "SIOC:B15:SSA1")
 
-## Tag log messages with IOC name
-epicsEnvSet("EPICS_IOC_LOG_CLIENT_INET","${IOC}")
+< $(TOP)/iocBoot/common/st.cmd.soft
 
-
-## The following two environment variables should be set for each SSA
-# epicsEnvSet("P",        "<pv_prefix>")
-# epicsEnvSet("IP",       "<ssa_ip_address>")
+## Run IOC shell script for each SSA
 #
-## Source the following file for each SSA after setting the two environment variables P and IP
-# < iocBoot/startup.modbus_RK_CA1300.cmd
-#
-## Load record instances for each SSA, after setting the environment variable P
-# dbLoadRecords("db/ssa_RK_CA1300.db", "P=$(P)")
-
 # SSA1: L1B:0210
-epicsEnvSet("P",        "ACCL:L1B:0210:SSA:")
-epicsEnvSet("IP",       "ssa-b15-rf0110")
-< iocBoot/common/startup.modbus_RK_CA1300.cmd
-dbLoadRecords("db/ssa_RK_CA1300_4.6kW.db", "P=$(P)")
-epicsThreadSleep(0.5)
+iocshLoad("$(TOP)/iocBoot/common/startup.RK_CA1300_4.6kW.iocsh", "PORT=L1B_0210, P=ACCL:L1B:0210:SSA:, IP=ssa-b15-rf0110")
 
-# =====================================================================
-# Load iocAdmin databases to support IOC Health and monitoring
-# =====================================================================
-dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC_P}")
-dbLoadRecords("db/iocAdminScanMon.db","IOC=${IOC_P}")
-dbLoadRecords("db/iocRelease.db","IOC=${IOC_P}")
-
-# =====================================================================
-# Load database for autosave status
-# =====================================================================
-dbLoadRecords("db/save_restoreStatus.db", "P=${IOC_P}:")
-
-
-# =====================================================================
-## Begin: Set up autosave/restore
-# =====================================================================
-
-# ============================================================
-# If all PVs don't connect continue anyway
-# ============================================================
-save_restoreSet_IncompleteSetsOk(1)
-
-# ============================================================
-# created save/restore backup files with date string
-# useful for recovery.
-# ============================================================
-save_restoreSet_DatedBackupFiles(1)
-
-# ============================================================
-# Where to find the list of PVs to save
-# ============================================================
-set_requestfile_path("${IOC_DATA}/${IOC}/autosave-req")
-
-# ============================================================
-# Where to write the save files that will be used to restore
-# ============================================================
-set_savefile_path("${IOC_DATA}/${IOC}/autosave")
-
-# ============================================================
-# Prefix that is use to update save/restore status database
-# records
-# ============================================================
-save_restoreSet_status_prefix("${IOC_P}:")
-
-## Restore datasets
-set_pass0_restoreFile("info_settings.sav")
-set_pass1_restoreFile("info_settings.sav")
-
-# =====================================================================
-# End: Setup autosave/restore
-# =====================================================================
-
-# =====================================================================
-# Channel Access Security:
-# This is required if you use caPutLog.
-# Set access security file
-# Load common LCLS Access Configuration File
-< ${ACF_INIT}
 
 iocInit()
 
@@ -110,9 +30,10 @@ caPutLogShow(2)
 seq(seq_ssa_CA1300, "P=ACCL:L1B:0210:SSA:")
 epicsThreadSleep(0.5)
 
-## Start autosave process:
-cd("${IOC_DATA}/${IOC}/autosave-req")
-makeAutosaveFiles()
-create_monitor_set("info_settings.req", 20, "")
+# Autosave start
+< $(TOP)/iocBoot/common/autosave_start.cmd
 
+cd $(TOP)
+
+# End of file
 
